8a1161d06767e087b020b61e557b4811ccc767ef
nadirs <nadirsampaoli@gmail.com>
2013-04-22 12:47:20 +0200

    Replace unused symbols with wildcards

--- Problems.hs
+++ Problems.hs
@@ -6,17 +6,17 @@ module Problems where
 --  1 Find the last element of a list
 myLast :: [a] -> a
 myLast (x:[]) = x
-myLast (x:xs) = myLast xs
+myLast (_:xs) = myLast xs
 
 --  2 Find the last but one element of a list
 myButLast :: [a] -> a
 myButLast (x:_:[]) = x
-myButLast (x:xs) = myButLast xs
+myButLast (_:xs) = myButLast xs
 
 --  3 Find the K'th element of a list. The first element in the list is number 1.
 myElementAt :: [a] -> Int -> a
 myElementAt (x:_) 1 = x
-myElementAt (x:xs) i = myElementAt xs (i-1)
+myElementAt (_:xs) i = myElementAt xs (i-1)
 
 --  4 Find the number of elements of a list
 myLength :: [a] -> Int
