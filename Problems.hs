01d5d696d709e5703aa704a02a55c9970c5c0bd1
nadirs <nadirsampaoli@gmail.com>
2013-04-22 12:44:24 +0200

    Add specs and solutions for problems 4-10

--- Problems.hs
+++ Problems.hs
@@ -18,3 +18,51 @@ myElementAt :: [a] -> Int -> a
 myElementAt (x:_) 1 = x
 myElementAt (x:xs) i = myElementAt xs (i-1)
 
+--  4 Find the number of elements of a list
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = (myLength xs) + 1
+
+--  5 Reverse a list
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
+
+--  6 Find out whether a list is a palindrome
+myIsPalindrome :: (Eq a) => [a] -> Bool
+myIsPalindrome [] = True
+myIsPalindrome [_] = True
+myIsPalindrome (x:xs) = (myIsPalindrome (init xs)) && (x == (last xs))
+
+--  7 Flatten a nested list structure
+data NestedList a = Elem a | List [NestedList a]
+
+myFlatten :: NestedList a -> [a]
+myFlatten (Elem a) = [a]
+myFlatten (List []) = []
+myFlatten (List (x:xs)) = (myFlatten x) ++ (myFlatten (List xs))
+
+--  8 Eliminate consecutive duplicates of list elements
+myCompress :: (Eq a) => [a] -> [a]
+myCompress [] = []
+myCompress [x] = [x]
+myCompress (x:ys@(y:zs))
+    | x == y    = myCompress ys
+    | otherwise = x : myCompress ys
+
+--  9 Pack consecutive duplicates of list elements into sublists
+myPack :: (Eq a) => [a] -> [[a]]
+myPack [] = []
+myPack [x] = [[x]]
+myPack xs = packer [] xs where
+    packer zs [] = zs : myPack []
+    packer [] (x:xs) = packer [x] xs
+    packer zs@(z:zzs) xs@(x:xxs)
+        | z == x    = packer (x:zs) xxs
+        | otherwise = zs : myPack xs
+
+-- 10 Run-length encoding of a list
+myRLEncode :: (Eq a) => [a] -> [(Int,a)]
+myRLEncode [] = []
+myRLEncode xs = zip (map length pack) (map head pack) where
+    pack = (myPack xs)
--- ProblemsSpec.hs
+++ ProblemsSpec.hs
@@ -21,3 +21,42 @@ main = hspec $ do
             myElementAt [3,-2,4,9,1] 3 `shouldBe` 4
             myElementAt "ciao" 2 `shouldBe` 'i'
             myElementAt [[4],[22],[9]] 1 `shouldBe` [4]
+
+    describe "myLength" $ do
+        it "finds the number of elements of a list" $ do
+            myLength [123, 456, 789] `shouldBe` 3
+            myLength "Hello, world!" `shouldBe` 13
+
+    describe "myReverse" $ do
+        it "reverses a list" $ do
+            myReverse "A man, a plan, a canal, panama!" `shouldBe`
+                "!amanap ,lanac a ,nalp a ,nam A"
+            myReverse [1,2,3,4] `shouldBe` [4,3,2,1]
+
+    describe "myIsPalindrome" $ do
+        it "finds out whether a list is a palindrome" $ do
+            myIsPalindrome [1,2,3] `shouldBe` False
+            myIsPalindrome "madamimadam" `shouldBe` True
+            myIsPalindrome [1,2,4,8,16,8,4,2,1] `shouldBe` True
+
+    describe "myFlatten" $ do
+        it "flatten a nested list structure" $ do
+            myFlatten (Elem 5) `shouldBe` [5]
+            myFlatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) `shouldBe` [1,2,3,4,5]
+
+    describe "myCompress" $ do
+        it "eliminates consecutive duplicates of list elements" $ do
+            myCompress "aaaabccaadeee" `shouldBe` "abcade"
+
+    describe "myPack" $ do
+        it "packs consecutive duplicates of list elements into sublists" $ do
+            myPack "" `shouldBe` []
+            myPack "a" `shouldBe` ["a"]
+            myPack "ab" `shouldBe` ["a", "b"]
+            myPack "aa" `shouldBe` ["aa"]
+            myPack "aab" `shouldBe` ["aa", "b"]
+            myPack "aaaabccaadeeee" `shouldBe` ["aaaa","b","cc","aa","d","eeee"]
+
+    describe "myRLEncode" $ do
+        it "does run-length encoding of a list" $ do
+            myRLEncode "aaaabccaadeeee" `shouldBe` [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
